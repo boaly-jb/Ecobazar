dce1dbd75053f1c28b2a70f9dfcee71f3aa0e6f2
CRAZY Designer <jababoaly@gmail.com>
2024-12-26 14:03:45 +0600

    add some img And do Responsive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 var mixer = mixitup('.mixer-all');
+
+
+
+new VenoBox({
+    selector: '.videoHome',
+});
+
+
+     
+  $(document).ready(function(){
+    $('#slider').slick({
+    // dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    default: 3000,
+    });
+  });
+
+// $(document).ready(function(){
+//     $('.your-class').slick({
+//       setting-name: setting-value
+//     });
+//   });
+      
